69ba22ab9e7733a1c914b61fee034ad19ec7ca68
sr <sunranlb@qq.com>
2023-05-04 22:22:21 +0800

    done

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,7 @@ public class Main {
     private static boolean checkBoard() {
         for (int i = 0; i < L; i++) {
             for (int j = 0; j < L; j++) {
-                int cnt = 0;
-                if (sCorner[i][j]) {
-                    cnt++;
-                }
-                if (sCorner[i][j + 1]) {
-                    cnt++;
-                }
-                if (sCorner[i + 1][j]) {
-                    cnt++;
-                }
-                if (sCorner[i + 1][j + 1]) {
-                    cnt++;
-                }
-                if (cnt != sBoard[i][j]) {
+                if (countAround(i, j) != sBoard[i][j]) {
                     return false;
                 }
             }
@@ -62,10 +49,168 @@ public class Main {
         sb.append(sCorner[i][j] ? SOLID : HOLLOW);
         System.out.println(sb);
     }
+
+    private static boolean traverse(int i, int j) {
+        //2421342244213321
+//        System.out.println(i + "," + j);
+        if (j == L) {
+            j = 0;
+            i++;
+            if (i == L) {
+                printBoard();
+                return true;
+            }
+        }
+        int cnt = countAround(i, j);
+        int num = sBoard[i][j];
+        if (cnt > num) {
+            return false;
+        } else if (cnt == num) {
+            return traverse(i, j + 1);
+        } else {
+            int diff = num - cnt;
+            if (i == 0) {
+                if (j == 0) {
+                    if (diff == 4) {
+                        boolean[] p = new boolean[]{true, true, true, true};
+                        op(true, i, j, p);
+                        if (traverse(i, j + 1)) {
+                            return true;
+                        }
+                        op(false, i, j, p);
+                    } else if (diff == 2) {
+                        boolean[] p = new boolean[4];
+                        for (int k = 0; k < 4; k++) {
+                            if (!sCorner[i + k / 2][j + k % 2]) {
+                                p[k] = true;
+                                for (int l = k + 1; l < 4; l++) {
+                                    if (!sCorner[i + l / 2][j + l % 2]) {
+                                        p[l] = true;
+                                        op(true, i, j, p);
+                                        if (traverse(i, j + 1)) {
+                                            return true;
+                                        }
+                                        op(false, i, j, p);
+                                        p[l] = false;
+                                    }
+                                }
+                                p[k] = false;
+                            }
+                        }
+                    } else {
+                        boolean init = diff == 3;
+                        boolean[] p = new boolean[]{init, init, init, init};
+                        for (int k = 0; k < 4; k++) {
+                            if (!sCorner[i + k / 2][j + k % 2]) {
+                                p[k] = !init;
+                                op(true, i, j, p);
+                                if (traverse(i, j + 1)) {
+                                    return true;
+                                }
+                                op(false, i, j, p);
+                                p[k] = init;
+                            }
+                        }
+                    }
+                } else {
+                    if (diff > 2) {
+                        return false;
+                    } else if (diff == 2) {
+                        boolean[] p = new boolean[]{false, true, false, true};
+                        op(true, i, j, p);
+                        if (traverse(i, j + 1)) {
+                            return true;
+                        }
+                        op(false, i, j, p);
+                    } else {
+                        boolean[] p = new boolean[]{false, true, false, false};
+                        op(true, i, j, p);
+                        if (traverse(i, j + 1)) {
+                            return true;
+                        }
+                        op(false, i, j, p);
+
+                        p = new boolean[]{false, false, false, true};
+                        op(true, i, j, p);
+                        if (traverse(i, j + 1)) {
+                            return true;
+                        }
+                        op(false, i, j, p);
+                    }
+                }
+            } else {
+                if (j == 0) {
+                    if (diff > 2) {
+                        return false;
+                    } else if (diff == 2) {
+                        boolean[] p = new boolean[]{false, false, true, true};
+                        op(true, i, j, p);
+                        if (traverse(i, j + 1)) {
+                            return true;
+                        }
+                        op(false, i, j, p);
+                    } else {
+                        boolean[] p = new boolean[]{false, false, true, false};
+                        op(true, i, j, p);
+                        if (traverse(i, j + 1)) {
+                            return true;
+                        }
+                        op(false, i, j, p);
+
+                        p = new boolean[]{false, false, false, true};
+                        op(true, i, j, p);
+                        if (traverse(i, j + 1)) {
+                            return true;
+                        }
+                        op(false, i, j, p);
+                    }
+                } else {
+                    if (diff > 1) {
+                        return false;
+                    }
+                    boolean[] p = new boolean[]{false, false, false, true};
+                    op(true, i, j, p);
+                    if (traverse(i, j + 1)) {
+                        return true;
+                    }
+                    op(false, i, j, p);
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void op(boolean add, int i, int j, boolean[] pos) {
+        for (int k = 0; k < 4; k++) {
+            if (pos[k]) {
+                sCorner[i + k / 2][j + k % 2] = add;
+            }
+        }
+    }
+
+    private static int countAround(int i, int j) {
+        int res = 0;
+        if (sCorner[i][j]) {
+            res++;
+        }
+        if (sCorner[i][j + 1]) {
+            res++;
+        }
+        if (sCorner[i + 1][j]) {
+            res++;
+        }
+        if (sCorner[i + 1][j + 1]) {
+            res++;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         testMain();
         Scanner sc = new Scanner(System.in);
         while (true) {
+            sCorner = new boolean[L + 1][L + 1];
+//        String ip = "2421342244213321";
             String ip = sc.nextLine();
             if (ip == null || ip.isEmpty()) {
                 break;
@@ -74,6 +219,9 @@ public class Main {
                 for (int i = 0; i < L * L; i++) {
                     sBoard[i / L][i % L] = ip.charAt(i) - '0';
                 }
+                traverse(0, 0);
+            } else {
+                System.out.println("Length error: " + ip.length());
             }
         }
     }
